d262c6532d40787c98daedfae9ef6028c819411f
2131121202-sudo <2131121202@ut.edu.vn>
2025-11-26 08:37:53 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import hashlib
+from datetime import datetime
 
 DB_NAME = "chat.db"
 
@@ -7,10 +8,10 @@ def init_db():
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     # Tạo bảng users
-    c.execute('''CREATE TABLE IF NOT EXISTS users
-                 (username TEXT PRIMARY KEY, password TEXT)''')
     c.execute('''CREATE TABLE IF NOT EXISTS messages
                  (sender TEXT, receiver TEXT, content TEXT, timestamp TEXT, type TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS users
+                 (username TEXT PRIMARY KEY, password TEXT)''')
     conn.commit()
     conn.close()
 
@@ -45,3 +46,20 @@ def get_all_users():
     rows = c.fetchall()
     conn.close()
     return [row[0] for row in rows]
+
+def save_message(sender, receiver, content, msg_type="TEXT"):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    c.execute("INSERT INTO messages VALUES (?, ?, ?, ?, ?)", 
+              (sender, receiver, content, time_now, msg_type))
+    conn.commit()
+    conn.close()
+
+def get_history(user):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('''SELECT * FROM messages WHERE receiver='ALL' OR receiver=? OR sender=?''', (user, user))
+    rows = c.fetchall()
+    conn.close()
+    return rows
